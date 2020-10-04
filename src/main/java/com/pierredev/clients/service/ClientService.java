ff42d9663f1652943c77b2ea83b7dfd95f7bdcda
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-04 20:27:57 -0300

    :sparkles: add client update

--- src/main/java/com/pierredev/clients/service/ClientService.java
+++ src/main/java/com/pierredev/clients/service/ClientService.java
@@ -2,6 +2,8 @@ package com.pierredev.clients.service;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -40,6 +42,19 @@ public class ClientService {
 		copyDtoEntity(dto, entity);
 		entity = clienteRepository.save(entity);
 		return new ClientDTO(entity);	
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = clienteRepository.getOne(id);
+			copyDtoEntity(dto, entity);
+			entity = clienteRepository.save(entity);
+			return new ClientDTO(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		
 	}
 	
 	private void copyDtoEntity(ClientDTO dto, Client entity) {
